8061b8cde86c8c893971e5c2cb59f7de76c59f69
vcqzero <1250857765@qq.com>
2019-01-09 16:31:56 +0800

    获取微信access token 改为从微信中控统一获取，避免各个应用独自获取时，造成过期的bug

--- module/Api/src/Service/Server/Weixiner.php
+++ module/Api/src/Service/Server/Weixiner.php
@@ -40,97 +40,21 @@ class Weixiner
      */
     public function getAccessToken()
     {
-        $Cache = $this->Cache;
-        $Cache->getOptions()->setTtl(self::EXPIRES_IN - 20);
-        
-        $access_token = $Cache->getItem(self::CACHE_KEY_WEIXIN_TOKEN);
-        if (empty($access_token))
-        {
-            //需要重新获取token
-            $url = 'https://api.weixin.qq.com/cgi-bin/token';
-            
-            $data = [
-                'appid'     => $this->appid,
-                'secret'    => $this->secret,
-                'grant_type'=> 'client_credential',
-            ];
-            
-            $res = MyCurl::get($url, $data);
-            if (!empty($res['errcode']))
-            {
-                $errcode = $res['errcode'];
-                $errmsg  = $res['errmsg'];
-                throw new \Exception("获取微信access_token 发生错误，错误代码为:$errcode, 错误信息：$errmsg");
-            }
-            $access_token = $res['access_token'];
-            $Cache->setItem(self::CACHE_KEY_WEIXIN_TOKEN, $access_token);
-        }
+        //get access token
+        $url = 'http://index.jjhycom.cn/api/access-token';
+        $res = MyCurl::get($url);
+        $access_token = $res['access_token'];
         return $access_token;
     }
     
-    public function getWxConfig($url)
+    public function getWxConfig($_url)
     {
-        $str="abcdrrlljokoptldiektldlyuiopasdfghjklzxcvbnm";
-        str_shuffle($str);
-        $noncestr       =substr(str_shuffle($str),5,10);
-        $jsapi_ticket   =$this->getJsapiTicket();
-        $timestamp      =time();
-        
-        $data=[
-            'jsapi_ticket'  =>$jsapi_ticket,
-            'noncestr'      =>$noncestr,
-            'timestamp'     =>$timestamp,
-            'url'           =>$url,
-        ];
-        $data_string=[];
-        foreach ($data as $key=>$val)
-        {
-            $data_string[]=$key . '=' . $val;
-        }
-        
-        $signature          =sha1(implode('&', $data_string));
-        $config = [
-            'appId' => $this->appid,
-            'timestamp' => $timestamp,
-            'nonceStr' => $noncestr,
-            'signature' => $signature,
-        ];
-        return $config;
-    }
-    
-    /**
-    * jsapi_ticket是公众号用于调用微信JS接口的临时票据。
-    * 正常情况下，jsapi_ticket的有效期为7200秒，
-    * 通过access_token来获取。
-    * 
-    * @param  
-    * @return        
-    */
-    public function getJsapiTicket()
-    {
-        $Cache = $this->Cache;
-        $Cache->getOptions()->setTtl(self::EXPIRES_IN - 20);
-        $jsapi_ticket = $Cache->getItem(self::CACHE_KEY_WEIXIN_JSAPI_TICKET);
-        //先确保access_token存在
-        //防止access_token更新之后jsapi使用时出错
-        $has_access_token_cached = $Cache->hasItem(self::CACHE_KEY_WEIXIN_TOKEN);
-        if (empty($has_access_token_cached) || empty($jsapi_ticket))
-        {
-            $access_token = $this->getAccessToken();
-            $url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=$access_token&type=jsapi";
-            $res = MyCurl::get($url);
-            
-            if (!empty($res['errcode']))
-            {
-                $errcode = $res['errcode'];
-                $errmsg  = $res['errmsg'];
-                throw new \Exception("获取微信jspai_ticket 发生错误，错误代码为:$errcode, 错误信息：$errmsg");
-            }
-            
-            $jsapi_ticket= $res['ticket'];
-            $Cache->setItem(self::CACHE_KEY_WEIXIN_JSAPI_TICKET, $jsapi_ticket);
-        }
-        return $jsapi_ticket;
+        //get wxconfig
+        $url = 'http://index.jjhycom.cn/api/wxconfig';
+        $data = ['url'=>$_url];
+        $res = MyCurl::post($data, $url);
+        $wxconfig= $res['wxconfig'];
+        return $wxconfig;
     }
     
     /**
